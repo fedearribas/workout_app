cf59e6f10d73db677ac29cd29d0b4d80ae3f301a
fedearribas <fede.arribas@gmail.com>
2016-03-22 16:23:11 +0000

    Fix flash messages bug

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -9,4 +9,20 @@ class Exercise < ActiveRecord::Base
   
   default_scope { where('workout_Date > ?', 7.days.ago).order(workout_date: :desc) }
   
+  def self.all_grouped_for_graph
+    @hash_grouped = all.group(:workout_date).sum(:duration_in_min)
+    @max_date = all.maximum(:workout_date)
+    @min_date = all.minimum(:workout_date)
+    @min_date.upto(@max_date) do |d|
+      if !@hash_grouped.has_key?(d)
+        @hash_grouped[d] = 0
+      end
+    end
+    @exer_grouped = Array.new
+    @hash_grouped.each do |k, v|
+      @exer_grouped << @hash_grouped = {:workout_date => k, :duration_in_min => v }
+    end
+    return @exer_grouped
+  end
+  
 end
